f115a780c2fcbb225f51a41b0b234d5c17cbcbae
Tom von Clef <tom@vonclef.com>
2013-09-14 23:54:49 -0400

    refactored function call to constructor setting

--- quib-console.js
+++ quib-console.js
@@ -40,18 +40,21 @@ ImageManipulator.prototype =
 
 var globalConsole;
 /** @constructor */
-function Console( )
+function Console( scale )
 {
+    this.dpiScale = (scale === 2) ? 2 : 1;
+
     var canvas = document.createElement("canvas");
     this.canvas = canvas;
-    this.canvas.width = 640;
-    this.canvas.height = 400;
-    this.dpiScale = 1;
+    this.canvas.width = 640 * this.dpiScale;
+    this.canvas.height = 400 * this.dpiScale;
     this.canvas.tabIndex = 0;
     this.ctx = this.canvas.getContext('2d');
     this.ctx.setTransform( 1, 0, 0, 1, 0, 0 );
     this.charImg = document.createElement("img");
-    this.charImg.setAttribute("src", "charmap.png");
+    this.charImg.setAttribute("src", (scale === 2)
+                              ? "charmap-2x.png"
+                              : "charmap.png");
 
     this.interval = 0;
     this.cursorEnabled = false;
@@ -89,6 +92,8 @@ function Console( )
     $(canvas).css("border-width", "5px");
     $(canvas).css("border-color", "#888888");
     $(canvas).css("border-style", "solid");
+    $(canvas).css("width", "640px");
+    $(canvas).css("height", "400px");
 
     this.cls();
 }
@@ -128,59 +133,6 @@ Console.prototype = {
         13: { width: 320, height: 200 }
     },
 
-    setDpiScale: function(scale)
-    {
-        if(typeof(scale) !== "number") {
-            throw "setDpiScale: invalid 'scale' argument.";
-        }
-
-        if(scale === this.dpiScale) {
-            return;
-        }
-        this.dpiScale = scale;
-
-        if(scale === 2) {
-            this.charImg.setAttribute("src", "charmap-2x.png");
-        }
-
-        var canvas = document.createElement("canvas");
-        this.canvas = canvas;
-        this.canvas.width = 640 * scale;
-        this.canvas.height = 400 * scale;
-        this.canvas.tabIndex = 0;
-        this.ctx = this.canvas.getContext('2d');
-        this.ctx.setTransform( 1, 0, 0, 1, 0, 0 );
-        this.reset(false);
-        this.hasFocus = false;
-
-        var self = this;
-
-        $(window).keydown( function( event ) {
-            if ( self.hasFocus ) {    
-                self.onKeyDown( event );
-                event.preventDefault();
-                return false;
-            }
-        });
-
-        $(canvas).click( function( event ) {
-            canvas.style.borderColor = "#008800";
-            $(canvas).focus();
-            self.hasFocus = true;
-            event.stopPropagation();
-        });
-
-        $(window).click( function( event ) {
-            self.hasFocus = false;
-            canvas.style.borderColor = "#888888";
-        });
-
-        $(canvas).css("border-width", "5px");
-        $(canvas).css("border-color", "#888888");
-        $(canvas).css("border-style", "solid");
-        $(canvas).css("width", "640px");
-        $(canvas).css("height", "400px");
-    },
 
     reset: function(testMode) 
     {
